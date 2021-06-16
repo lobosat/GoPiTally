1f2b87b1a105046412e80b24ba60b629697dc3b9
lou <luis.navarro.iii@gmail.com>
2021-06-16 12:52:35 -0500

    v0.22 - Fixed doap.sh by changing order of service stop and start.

--- usr/local/bin/doap.sh
+++ usr/local/bin/doap.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
 case $1 in
-   on)
-        systemctl disable pitally
-        systemctl stop hostapd
-        systemctl stop dnsmasq
-        systemctl stop dhcpcd
-        cp /usr/local/etc/ap/enabled/dhcpcd.conf /etc/dhcpcd.conf
-        cp /usr/local/etc/ap/enabled/hostapd.conf /etc/hostapd/hostapd.conf
-        cp /usr/local/etc/ap/enabled/dnsmasq.conf /etc/dnsmasq.conf
-        systemctl daemon-restart
-        systemctl enable hostapd
-        systemctl start hostapd
-        systemctl start dnsmasq
-        systemctl start dhcpcd
+on)
+  systemctl stop pitally
+  cp /usr/local/etc/ap/enabled/dnsmasq.conf /etc/dnsmasq.conf
+  cp /usr/local/etc/ap/enabled/dhcpcd.conf /etc/dhcpcd.conf
+  cp /usr/local/etc/ap/enabled/hostapd.conf /etc/hostapd/hostapd.conf
+
+  systemctl daemon-reload
+  systemctl start dnsmasq
+  systemctl restart dhcpcd
+  systemctl unmask hostapd
+  systemctl start hostapd
   ;;
 
-   off)
-        systemctl stop hostapd
-        systemctl disable hostapd
-        systemctl stop dnsmasq
-        systemctl stop dhcpcd
-        cp /usr/local/etc/ap/disabled/dhcpcd.conf /etc/dhcpcd.conf
-        cp /usr/local/etc/ap/disabled/hostapd.conf /etc/hostapd/hostapd.conf
-        cp /usr/local/etc/ap/disabled/dnsmasq.conf /etc/dnsmasq.conf
-        systemctl daemon-restart
-        systemctl start dnsmasq
-        systemctl start dhcpcd
-        systemctl enable pitally
-        systemctl start pitally
-   ;;
+off)
+  systemctl stop hostapd
+  systemctl mask hostapd
+  systemctl stop dnsmasq
+  cp /usr/local/etc/ap/disabled/dhcpcd.conf /etc/dhcpcd.conf
+  cp /usr/local/etc/ap/disabled/hostapd.conf /etc/hostapd/hostapd.conf
+  cp /usr/local/etc/ap/disabled/dnsmasq.conf /etc/dnsmasq.conf
+  systemctl daemon-reload
+  systemctl restart dhcpcd
+  systemctl start pitally
+  ;;
 esac
